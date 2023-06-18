a6ecdec4370d07ef5a9e8c0054340d9076681099
Dheeraj Singh Bhadoria <dheerubhadoria@gmail.com>
2023-06-18 22:41:18 +0530

    Starter project

---
+++ source-code/starter-project/lib/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_internationalization_localization/internationalization.dart';
+import 'package:flutter_internationalization_localization/localization.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.lightBlueAccent,
+              width: 300,
+              height: 50,
+              child: InkWell(child: Center(child: Text("Internationalization Example", style: TextStyle(color: Colors.white),),), onTap: (){
+                
+				
+              },),
+            ),
+            SizedBox(height: 10,),
+            Container(
+              color: Colors.lightBlueAccent,
+              width: 300,
+              height: 50,
+              child: InkWell(child: Center(child: Text("Localization Example ( Login Screen )", style: TextStyle(color: Colors.white)),), onTap: (){
+            
+			
+			
+              },),
+            )
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ source-code/starter-project/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_internationalization_localization/home.dart';
+import 'package:flutter_internationalization_localization/localization.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'internationalization.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      //home: const Internationalization(title: 'Internationlization Example'),
+      home: Home(title: "Multilingual App"),
+      localizationsDelegates: [
+        AppLocalizations.delegate, // Add this line
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'), // English
+        Locale('hi'), // Spanish
+      ],
+    );
+  }
+}
