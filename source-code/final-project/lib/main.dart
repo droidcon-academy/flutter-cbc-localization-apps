760f14a0dfe013f52f72c6b48d67a15fb03cc021
Dheeraj Singh Bhadoria <dheerubhadoria@gmail.com>
2023-06-24 20:48:32 +0530

    IDE Update

--- source-code/final-project/lib/main.dart
+++ source-code/final-project/lib/main.dart
@@ -7,7 +7,9 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'internationalization.dart';
 
 void main() {
+
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
